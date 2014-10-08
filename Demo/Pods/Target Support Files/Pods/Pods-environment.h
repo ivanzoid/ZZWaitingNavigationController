448f07539f23646f116c346c1cd1c1cf137a06f3
Ivan Zezyulya <ivan@zezyulya.com>
2014-10-09 02:38:18 +0700

    Update demo project

--- Demo/Pods/Target Support Files/Pods/Pods-environment.h
+++ Demo/Pods/Target Support Files/Pods/Pods-environment.h
@@ -16,5 +16,5 @@
 #define COCOAPODS_POD_AVAILABLE_ZZWaitingNavigationController
 #define COCOAPODS_VERSION_MAJOR_ZZWaitingNavigationController 1
 #define COCOAPODS_VERSION_MINOR_ZZWaitingNavigationController 0
-#define COCOAPODS_VERSION_PATCH_ZZWaitingNavigationController 0
+#define COCOAPODS_VERSION_PATCH_ZZWaitingNavigationController 1
 
